64268b9f8b235cfcee92f6050dec56bb67de009a
Arden Shackelford <arden@ardens.tech>
2022-12-13 23:49:57 -0600

    A couple additional tweaks; Finally got a manual span generated, just can't get exporting to work;

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,17 +1,26 @@
 <script>
-    import {tracerWithZone} from "../tracing"
+
+    import opentelemetry from "@opentelemetry/api";
     import {context, trace} from "@opentelemetry/api";
 
     let count = 0
+    const tracer = opentelemetry.trace.getTracer("counterTracer")
 
     const increment = () => {
-        const incrementSpan = tracerWithZone.startSpan('Increment Counter');
-        context.with(trace.setSpan(context.active(), incrementSpan), () => {
-                trace.getSpan(context.active()).addEvent('Begin Increment');
-                count += 1
-                trace.getSpan(context.active()).addEvent('Increment Success');
-            }
-        )
+        tracer.startActiveSpan('Increment Counter', span => {
+            console.log("Triggered increment!")
+            trace.getSpan(context.active()).addEvent('Begin Increment');
+            count += 1
+            trace.getSpan(context.active()).addEvent('Increment Success');
+            span.end()
+        });
+        // context.with(trace.setSpan(context.active(), incrementSpan), () => {
+        //         console.log("Triggered increment!")
+        //         trace.getSpan(context.active()).addEvent('Begin Increment');
+        //         count += 1
+        //         trace.getSpan(context.active()).addEvent('Increment Success');
+        //     }
+        // )
     }
 </script>
 
--- src/tracing.js
+++ src/tracing.js
@@ -1,26 +1,35 @@
 import {
     BatchSpanProcessor, ConsoleSpanExporter, WebTracerProvider,
 } from '@opentelemetry/sdk-trace-web';
+import {SemanticResourceAttributes} from "@opentelemetry/semantic-conventions";
 import {OTLPTraceExporter} from '@opentelemetry/exporter-trace-otlp-http';
 import {XMLHttpRequestInstrumentation} from '@opentelemetry/instrumentation-xml-http-request';
 import {ZoneContextManager} from '@opentelemetry/context-zone';
 import {registerInstrumentations} from '@opentelemetry/instrumentation';
 import {DocumentLoadInstrumentation} from "@opentelemetry/instrumentation-document-load";
+import {Resource} from "@opentelemetry/resources";
 
-const providerWithZone = new WebTracerProvider();
+const resource = Resource.default().merge(new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: import.meta.env.VITE_OTEL_SERVICE_NAME,
+}));
+
+const providerWithZone = new WebTracerProvider({
+    resource: resource,
+});
 
 console.log("Reached tracing!")
 console.log(`Here is the env for OTel endpoint: ${import.meta.env.VITE_OTEL_EXPORTER_OTLP_ENDPOINT}`)
 
 // env var parsing: https://vitejs.dev/guide/env-and-mode.html
-providerWithZone.addSpanProcessor(new BatchSpanProcessor(new OTLPTraceExporter(
-    {
-        serviceName: import.meta.env.VITE_OTEL_SERVICE_NAME,
-        url: import.meta.env.VITE_OTEL_EXPORTER_OTLP_ENDPOINT,
-        insecure: true
-    }
-)));
-providerWithZone.addSpanProcessor(new BatchSpanProcessor(new ConsoleSpanExporter()));
+providerWithZone.addSpanProcessor(new BatchSpanProcessor(new OTLPTraceExporter({
+    serviceName: import.meta.env.VITE_OTEL_SERVICE_NAME,
+    url: import.meta.env.VITE_OTEL_EXPORTER_OTLP_ENDPOINT,
+    insecure: false
+})));
+
+providerWithZone.addSpanProcessor(new BatchSpanProcessor(
+    new ConsoleSpanExporter()
+));
 
 providerWithZone.register({
     contextManager: new ZoneContextManager()
@@ -29,11 +38,9 @@ providerWithZone.register({
 providerWithZone.register();
 
 registerInstrumentations({
-    instrumentations: [
-        new XMLHttpRequestInstrumentation({
-            propagateTraceHeaderCorsUrls: ['http://localhost:8090']
-        }),
-        new DocumentLoadInstrumentation],
+    instrumentations: [new XMLHttpRequestInstrumentation({
+        propagateTraceHeaderCorsUrls: ['http://localhost:8090']
+    }), new DocumentLoadInstrumentation],
 });
 
 const tracerWithZone = providerWithZone.getTracer("")
